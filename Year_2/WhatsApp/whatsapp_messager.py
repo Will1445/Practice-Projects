b1e6e64012b7ed711ac757a3b75c3fa508fdbc6e
Will <will.crow@sky.com>
2025-05-12 11:21:05 +0100

    update

--- Year_2/WhatsApp/whatsapp_messager.py
+++ Year_2/WhatsApp/whatsapp_messager.py
@@ -3,16 +3,14 @@ import pyautogui
 import time
 
 # Phone number (including country code) and message
-phone_number = "+447778215119"  # Replace with the recipient's phone number
-message = "I think so, I just sent you this message using a python script"
+phone_number = "+447541717922"  
+message = "Wadup"
 
-# Send message instantly
+
 kit.sendwhatmsg_instantly(phone_number, message)
 
-# Wait for a few seconds to allow WhatsApp Web to finish typing
 time.sleep(1)
 
-# Simulate pressing the "Enter" key to send the message
 pyautogui.press('enter')
 
 
